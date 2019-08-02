ffcc14bb25211006ce64286afd058d66c5ace9f4
vardy <jarred.vardy@gmail.com>
2019-08-02 14:49:07 +0800

    Initial commit

---
+++ client.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Provides interface for accessing S3 API connection
+    as a custom Client object.
+"""
+
+# Standard library
+import os
+
+# Third-party sources
+import boto3  # S3 API interface
+
+class Client:
+    __S3_Endpoint = os.getenv('S3_Endpoint')
+    __S3_Region = os.getenv('S3_Region')
+    __S3_Access_Key = os.getenv('S3_Access_Key')
+    __S3_Secret_Key = os.getenv('S3_Secret_Key')
+    __S3_Directory_Name = os.getenv('S3_Directory_Name')
+    __Volumes_To_Backup = os.getenv('Volumes_To_Backup')
+
+    __S3_Client = None
+    
+    def __init__(self):
+        session = boto3.session.Session()
+        s3_client = session.client(
+            service_name='s3',
+            aws_access_key_id=self.__S3_Access_Key,
+            aws_secret_access_key=self.__S3_Secret_Key,
+            region_name=self.__S3_Region,
+            endpoint_url=self.__S3_Endpoint,
+        )
+
+        self.__S3_Client = s3_client
+    
+    def get_s3_client(self):
+        return self.__S3_Client
+
+    def get_volumes_to_backup(self):
+        return self.__Volumes_To_Backup
+
+    def get_directory_name(self):
+        return self.__S3_Directory_Name
---
+++ main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Main class of DockerVolumeBackup.
+    Carries out procedure of retrieving data from Docker
+    volumes specified and snapshotting data backups.
+"""
+
+# Standard library
+import os
+import sys
+import subprocess  # Module used for shell commands
+
+# Local source
+from client import Client
+
+def main():
+    client = Client()
+
+    if client.get_s3_client() is not None:
+        volumes_to_backup = client.get_volumes_to_backup()
+        if not ''.__eq__(volumes_to_backup):
+            arr_volumes = [x.strip() for x in volumes_to_backup.split(',')]
+            # If metafile does not exist in directory, generate metafile
+            for vol in arr_volumes:
+                # Check if volume exists, if not throw error and move on
+                # Check if entry for volume exists in metafile volumes array
+                #   If entry exists move on
+                #   else add entry, generate snapshotID, init snapshot num to 0
+                # Check metafile
+                #   Increment snapshot number
+                #   If snapshot number > interval, create new file and save first snapshot, reset snapshot num, generate new snapshot id
+                #   else overwrite current snapshot with new interval number
+                #
+                ## S3 Directory Structure
+                #
+                # s3_directory/
+                #     metafile
+                #     volume_name/
+                #         snapshotID_Interval_DateTime
+                #     volume_name/
+                #         snapshotID_Interval_DateTime
+                #         snapshotID_Interval_DateTime
+                #
+                ## Metafile Structure (JSON)
+                #
+                # {
+                #     "volumes": [
+                #         {
+                #              "volume_name": "loyalbot_db-data",
+                #              "current_snapshot_id": "uuid",
+                #              "snapshot_num": 1
+                #         },
+                #         {
+                #              "volume_name": "onebillionquacks_db-data",
+                #              "current_snapshot_id": "uuid",
+                #              "snapshot_num": 1
+                #         }
+                #     ]
+                # }
+        else:
+            print('ERROR: No volumes were specified. Exiting.')
+            sys.exit(1)
+    else:
+        print('ERROR: Client failed to be instantiated. Exiting.')
+        sys.exit(1)
+        return
+
+if __name__ =='__main__':
+    main()
