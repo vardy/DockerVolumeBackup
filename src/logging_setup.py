e4491f9cd40032d62ffad628d16514230746de62
vardy <jarred.vardy@gmail.com>
2019-08-02 17:51:34 +0800

    Moved logger setup to its own module

---
+++ src/logging_setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Basic setup for root logger
+"""
+
+# Standard library
+import os
+import logging
+
+log_level = os.environ.get('Log_Level', 'INFO').upper()
+
+logging.basicConfig(
+    format='%(asctime)s: %(levelname)s: %(message)s', 
+    level=log_level
+)
